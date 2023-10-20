e07be6cdf95334a29b593703b3861f52899cc370
Henry <henydoudoc@gmail.com>
2023-10-20 22:27:27 +0200

    sript.js - Spect 5 -Optimización del codigo - Spring 1

--- game.php
+++ game.php
@@ -1,6 +1,6 @@
 <?php
-if ($_SESSION['lang'] == 'es') {
-}
+/*if ($_SESSION['lang'] == 'es') {
+}*/
 $contenido = file_get_contents('questions/catalan_1.txt');
 $lineas = explode("\n", $contenido);
 
@@ -21,7 +21,7 @@ foreach ($lineas as $linea) {
         if (!empty($pregunta_actual)) {
             $preguntas[] = $pregunta_actual;
         }
-        $pregunta_actual = ['pregunta' => $linea=substr($linea, 2), 'respuestas' => []];
+        $pregunta_actual = ['pregunta' => $linea = substr($linea, 2), 'respuestas' => []];
     }
 }
 shuffle($preguntas);
@@ -71,7 +71,7 @@ $preguntasAleatorias = array_slice($preguntas, 0, 3);
 
     <div class="questionHidden" id="question3">
         <h2><?php echo $preguntasAleatorias[2]['pregunta']; ?></h2>
-        
+        <form id="form3">
         <ul>
             <?php foreach ($preguntasAleatorias[2]['respuestas'] as $respuesta) : ?>
                 <li>
@@ -80,7 +80,8 @@ $preguntasAleatorias = array_slice($preguntas, 0, 3);
             <?php endforeach; ?>
         </ul>
         <br>
-        <div id="feedback2"></div>
+        </form>
+        <div id="feedback3"></div>
     </div>
 
     <form action="game.js">
@@ -92,53 +93,43 @@ $preguntasAleatorias = array_slice($preguntas, 0, 3);
 
 
     <script>
-        // JavaScript para manejar la interacción del jugador
-   
-        // JavaScript para manejar la interacción del jugador
-        document.getElementById("form1").addEventListener("change", function () {
-            var selectedAnswer = document.querySelector('input[name="respuesta1"]:checked');
-            if (selectedAnswer) {
-                var playerAnswer = selectedAnswer.value;
-                var firstChar = playerAnswer.charAt(0);
-                var isCorrect = (firstChar === '+');
-                if (isCorrect) {
-                    document.getElementById("feedback1").innerHTML = "Resposta correcta. Avançant a la següent pregunta...";
-                    // Deshabilita todas las respuestas de la pregunta 1
-                    document.getElementById("question2").classList.remove("questionHidden");
-                    var answers = document.querySelectorAll('.answer1');
-                    for (var i = 0; i < answers.length; i++) {
-                        answers[i].disabled = true;
-                    }
-                } else {
-                    document.getElementById("feedback1").innerHTML = "Resposta incorrecta. Intenteu-ho de nou.";
-                }
-            } else {
-                document.getElementById("feedback1").innerHTML = "";
-            }
-        });
+        // Info de la estructura HTLM
+        let respuesta1=comprobarRespuesta("form1", "respuesta1", "feedback1", "question2", "answer1");
+        let respuesta2=comprobarRespuesta("form2", "respuesta2", "feedback2", "question3", "answer2");
+        let respuesta3=comprobarRespuesta("form3", "respuesta3", "feedback3", "question4", "answer3");
 
-        document.getElementById("form2").addEventListener("change", function () {
-            var selectedAnswer = document.querySelector('input[name="respuesta2"]:checked');
-            if (selectedAnswer) {
-                var playerAnswer = selectedAnswer.value;
-                var firstChar = playerAnswer.charAt(0);
-                var isCorrect = (firstChar === '+');
-                if (isCorrect) {
-                    document.getElementById("feedback2").innerHTML = "Resposta correcta. Avançant a la següent pregunta...";
-                    // Deshabilita todas las respuestas de la pregunta 1
-                    document.getElementById("question3").classList.remove("questionHidden");
-                    var answers = document.querySelectorAll('.answer2');
-                    for (var i = 0; i < answers.length; i++) {
-                        answers[i].disabled = true;
-                    }
-                } else {
-                    document.getElementById("feedback2").innerHTML = "Resposta incorrecta. Intenteu-ho de nou.";
-                }
-            } else {
-                document.getElementById("feedback2").innerHTML = "";
-            }
-        });
 
+        function comprobarRespuesta(formId, answerName, feedbackId, nextQuestionId, answerClass) {
+            document.getElementById(formId).addEventListener("change", function() {
+                var selectedAnswer = document.querySelector(`input[name="${answerName}"]:checked`);
+                var feedbackElement = document.getElementById(feedbackId);
+                var nextQuestionElement = document.getElementById(nextQuestionId);
+                var answers = document.querySelectorAll(`.${answerClass}`);
+                
+                if (formId!=="form3") {
+                    if (selectedAnswer) {
+                        var playerAnswer = selectedAnswer.value;
+                        var firstChar = playerAnswer.charAt(0);
+                        var isCorrect = (firstChar === '+');
+                        if (isCorrect) {
+                            feedbackElement.innerHTML = "Resposta correcta. Avançant a la següent pregunta...";
+                            nextQuestionElement.classList.remove("questionHidden");
+                            
+                            for (var i = 0; i < answers.length; i++) {
+                                answers[i].disabled = true;
+                            }
+                        } else {
+                            feedbackElement.innerHTML = "Resposta incorrecta. Intenteu-ho de nou.";
+                        }
+                    } else {
+                        feedbackElement.innerHTML = "";
+                    }
+                }else{
+                    console.log("---------------->consol")
+                    
+                }
+            });
+        }
     </script>
 </body>
 
