c125cf50e033cec5a336a8b1edd2042e0053403a
Henry <henydoudoc@gmail.com>
2023-10-20 22:30:10 +0200

    sript.js - Spect 5 -Optimización del codigo DEFINITIVO - Spring 1

--- game.php
+++ game.php
@@ -92,44 +92,7 @@ $preguntasAleatorias = array_slice($preguntas, 0, 3);
     </form>
 
 
-    <script>
-        // Info de la estructura HTLM
-        let respuesta1=comprobarRespuesta("form1", "respuesta1", "feedback1", "question2", "answer1");
-        let respuesta2=comprobarRespuesta("form2", "respuesta2", "feedback2", "question3", "answer2");
-        let respuesta3=comprobarRespuesta("form3", "respuesta3", "feedback3", "question4", "answer3");
-
-
-        function comprobarRespuesta(formId, answerName, feedbackId, nextQuestionId, answerClass) {
-            document.getElementById(formId).addEventListener("change", function() {
-                var selectedAnswer = document.querySelector(`input[name="${answerName}"]:checked`);
-                var feedbackElement = document.getElementById(feedbackId);
-                var nextQuestionElement = document.getElementById(nextQuestionId);
-                var answers = document.querySelectorAll(`.${answerClass}`);
-                
-                if (formId!=="form3") {
-                    if (selectedAnswer) {
-                        var playerAnswer = selectedAnswer.value;
-                        var firstChar = playerAnswer.charAt(0);
-                        var isCorrect = (firstChar === '+');
-                        if (isCorrect) {
-                            feedbackElement.innerHTML = "Resposta correcta. Avançant a la següent pregunta...";
-                            nextQuestionElement.classList.remove("questionHidden");
-                            
-                            for (var i = 0; i < answers.length; i++) {
-                                answers[i].disabled = true;
-                            }
-                        } else {
-                            feedbackElement.innerHTML = "Resposta incorrecta. Intenteu-ho de nou.";
-                        }
-                    } else {
-                        feedbackElement.innerHTML = "";
-                    }
-                }else{
-                    console.log("---------------->consol")
-                    
-                }
-            });
-        }
+    <script src="script.js">
     </script>
 </body>
 
