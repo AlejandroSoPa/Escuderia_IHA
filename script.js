d25748cec78d41c344f0dafaec53af3c7b8d745f
Henry <henydoudoc@gmail.com>
2023-10-23 16:29:25 +0200

    game.php - Spect 7 Finished - Spring 1

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-let respuesta1=comprobarRespuesta("form1", "respuesta1", "feedback1", "question2", "answer1");
-let respuesta2=comprobarRespuesta("form2", "respuesta2", "feedback2", "question3", "answer2");
-let respuesta3=comprobarRespuesta("form3", "respuesta3", "feedback3", "question4", "answer3");
+let respuesta1 = comprobarRespuesta("form1", "respuesta1", "feedback1", "question2", "answer1");
+let respuesta2 = comprobarRespuesta("form2", "respuesta2", "feedback2", "question3", "answer2");
+let respuesta3 = comprobarRespuesta("form3", "respuesta3", "feedback3", "question4", "answer3");
 
-
+var preguntasCorrectas = 0;
 function comprobarRespuesta(formId, answerName, feedbackId, nextQuestionId, answerClass) {
-    document.getElementById(formId).addEventListener("change", function() {
+    document.getElementById(formId).addEventListener("change", function () {
         var selectedAnswer = document.querySelector(`input[name="${answerName}"]:checked`);
         var feedbackElement = document.getElementById(feedbackId);
         var nextQuestionElement = document.getElementById(nextQuestionId);
@@ -16,13 +16,15 @@ function comprobarRespuesta(formId, answerName, feedbackId, nextQuestionId, answ
             var isCorrect = (firstChar === '+');
             if (isCorrect) {
                 feedbackElement.innerHTML = "Resposta correcta. Avançant a la següent pregunta...";
+                preguntasCorrectas++;
                 nextQuestionElement.classList.remove("questionHidden");
                 //Bloquear respuesta una vez contestada
-                for (var i = 0; i <= answers.length; i++) {
+                for (var i = 0; i < answers.length; i++) {
                     answers[i].disabled = true;
                 }
+                console.log(preguntasCorrectas);
             } else {
-                
+
                 feedbackElement.innerHTML = "Resposta incorrecta.";
                 var btnInici = document.getElementById('btnInici');
                 //Mostrar boton de inicio si falla la respuesta
@@ -31,7 +33,7 @@ function comprobarRespuesta(formId, answerName, feedbackId, nextQuestionId, answ
 
                 }
                 //Bloquear respuesta una vez contestada
-                for (var i = 0; i <= answers.length; i++) {
+                for (var i = 0; i < answers.length; i++) {
                     answers[i].disabled = true;
                 }
 
@@ -39,6 +41,6 @@ function comprobarRespuesta(formId, answerName, feedbackId, nextQuestionId, answ
         } else {
             feedbackElement.innerHTML = "";
         }
-        
+
     });
 }
