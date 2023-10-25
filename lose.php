0f782196042598c97f77e51dd4d3a2cd7f16edce
AlejandroSoPa <asoladopalomino.cf@iesesteveterradas.cat>
2023-10-25 17:02:08 +0200

    DIAGRAMAS + lose.php

--- lose.php
+++ lose.php
@@ -1,18 +1,26 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="styles.css">
 </head>
 
 <body>
     <audio autoplay>
-        <source src="audio/GameOver.mp3" type="audio/mpeg">
+        <source src="audio/exit.mp3" type="audio/mpeg">
     </audio>
-    <h1>Has perdido</h1>
-    <h2>Vuelve a intentar-lo </h2>
+    <?php
+    echo "<h1>Game Over</h1> <!-- Cambiar por varible-->";
+    ?>
+    <form action="index.php">
+        <input id="btnIniciWin" visibility:visible type="submit" value="Tornar a l' inici"><!-- Cambiar por varible-->
+    </form>
+    <form action="ranking.php">
+        <input id="btntRanking" visibility:visible type="submit" value="Pantalla Ranking"><!-- Cambiar por varible-->
+    </form>
 </body>
 
 </html>
