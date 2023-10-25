38a65cc739f2691fc980d43df1d06d2008eb9245
AlejandroSoPa <asoladopalomino.cf@iesesteveterradas.cat>
2023-10-25 18:36:27 +0200

    Commit finish spec15

--- win.php
+++ win.php
@@ -5,28 +5,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="styles.css">
+    <script src="manageButtons.js"></script>
 </head>
 <body>
     <?php
-        echo "<h1>Felicidades! Has Ganadado</h1> <!-- Cambiar por varible-->";
+        echo "<h1>Felicidades! Has Ganado</h1> <!-- Cambiar por varible-->";
     ?>
-    <button onclick="document.getElementById('aviso').setAttribute('data-on','on')">Publicar</button> 
+    <button id="publish" onclick="document.getElementById('aviso').setAttribute('data-on','on')">Publicar</button> 
     <div class="panel" id="aviso" data-on="off" onclick="this.setAttribute('data-on','on')">  
         <div>
             <?php
-                echo"<form action='index.php' method='post'>";
+                echo"<form action='' method='post'>";
                     echo"<p>Introduce tu nombre</p> <!-- Cambiar por varible-->";
                     echo"<input type='text' name='name' id='name' cols='54' rows='5' required></input><br>";
                     echo"<input type='number' name='point' id='point' cols='40' rows='5' style='display: none;' value='18'></input>";
                     echo"<input type='submit'>";
                 echo"</form>";
-            
                 session_start();
                 $texto = preg_replace('/\s+/', "", $_POST["name"]);
                 if (isset($texto)) {
                     //Código de validación de datos
                     file_put_contents("records.txt", 
-                    file_get_contents("records.txt").trim($text).",".$_POST["point"].",".session_id()."\n");
+                    file_get_contents("records.txt").trim($texto).",".$_POST["point"].",".session_id()."\n");
                     session_destroy();
                 }
             ?>
