47c856e7d80ef9b9cbc6497a4ae0cde2b1517336
AlejandroSoPa <asoldadopalomino.cf@iesesteveterradas.cat>
2023-10-31 16:14:00 +0100

    Trying to upgrade the redirect

--- resources/setNextLevel.php
+++ resources/setNextLevel.php
@@ -15,10 +15,10 @@
             $_SESSION['counter'] += 3;
             if ($_SESSION['counter'] == 18) {
                 echo "<form action='../win.php' method='post'>";
-                    echo "<input type='number' id='inicio' name='inicio' value=''></input>";
-                    echo "<input type='number' id='actual' name='actual' value=''></input>";
-                    echo "<input type='text' id='crono' name='crono' value=''></input>";
-                    echo "<input type='submit' id='game_won' name='game_won' value='' onclick='empezarDetener(this);'></input>";
+                    echo "<input type='number' id='inicio' name='inicio' value='' style='visibility:hidden;'></input>";
+                    echo "<input type='number' id='actual' name='actual' value='' style='visibility:hidden;'></input>";
+                    echo "<input type='text' id='crono' name='crono' value='' style='visibility:hidden;'></input>";
+                    echo "<input type='submit' id='game_won' name='game_won' value='' onclick='empezarDetener(this);' style='visibility:hidden;'></input>";
                 echo "</form>";
                 echo "<script> showData(); </script>";
             } else {
@@ -27,7 +27,7 @@
             }
         } else {
             echo "<form action='../lose.php' method='post'>";
-                echo "<input type='submit' id='game_lose' name='game_lose' value='' onclick='empezarDetener(this);'></input>";
+                echo "<input type='submit' id='game_lose' name='game_lose' value='' onclick='empezarDetener(this);' style='visibility:hidden;'></input>";
             echo "</form>";
         }
     ?>
