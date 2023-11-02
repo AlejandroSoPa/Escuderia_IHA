98fa8cda15c157f0a4e06135b9348371dfcdb57a
IFurio <ifuriomartin.cf@iesesteveterradas.cat>
2023-11-02 20:21:00 +0100

    In progress create.php

---
+++ resources/checkForm.php
@@ -0,0 +1,6 @@
+<?php
+    if(!isset($_POST['questionLang']) || !isset($_POST['questionLevel']) || !isset($_POST['question']) || !isset($_POST['correctAnswer']) 
+    || !isset($_POST['incorrectAnswer1']) || !isset($_POST['incorrectAnswer2']) || !isset($_POST['incorrectAnswer3'])) {
+        // devolver un feedback de rellenar todos los datos
+    }
+?>
