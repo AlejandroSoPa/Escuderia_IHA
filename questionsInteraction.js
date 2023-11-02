825edba934d13e0b87a0b345f0ac920a5eeff4b8
Isaac <ifuriomartin.cf@iesesteveterradas.cat>
2023-11-02 01:57:09 +0100

    Comments translated to english

--- questionsInteraction.js
+++ questionsInteraction.js
@@ -2,14 +2,12 @@ let respuesta1 = checkAnswer("form1", "respuesta1", "feedback1", "feedback11", "
 let respuesta2 = checkAnswer("form2", "respuesta2", "feedback2", "feedback22", "question3", "answer2");
 let respuesta3 = checkAnswer("form3", "respuesta3", "feedback3", "feedback33", "question4", "answer3");
 
-// Suponiendo que obtienes los elementos de audio por sus IDs
 var audioCorrecto = document.getElementById('audioCorrecto');
 var audioIncorrecto = document.getElementById('audioIncorrecto');
 var audioPublic = document.getElementById('audioPublic');
 var correctAnswers = 0;
-//Scroll
-var currentQuestion = 1; // Pregunta actual
-var totalQuestions = 3; // Número total de preguntes
+var currentQuestion = 1; 
+var totalQuestions = 3;
 
 var countDownTimer;
 var time = initialTime;
@@ -77,8 +75,8 @@ function checkAnswer(formId, answerName, feedbackId, feedbackId2, nextQuestionId
 function showNextQuestion(nextQuestionId) {
     if (currentQuestion < totalQuestions) {
         var nextQuestionElement = document.getElementById(nextQuestionId);
-        nextQuestionElement.scrollIntoView({ behavior: "smooth" }); // Desplaçar-se a la següent pregunta
-        currentQuestion++; // Incrementa la pregunta actual
+        nextQuestionElement.scrollIntoView({ behavior: "smooth" }); // Scroll to next question
+        currentQuestion++;
     }
 }
 
@@ -174,32 +172,29 @@ function publicWildcard() {
         numberOfAnswers = document.getElementById("list1").getElementsByTagName("li").length;
         let correctAnswer = returnCorrectAnswerPosition("answer1");
         setTimeout(function() {
-            // Ocultar la animación de carga después de un tiempo de espera
+            // After a time of 15 seconds hide the loading animation and the feedback and create the svg chart
             document.getElementById('loading').style.display = 'none';
             document.getElementById('wildcardFeedback').style.display = 'none';
             createChart(numberOfAnswers, correctAnswer);
-            // Realiza la acción deseada aquí, por ejemplo, cargar contenido o realizar una solicitud AJAX.
         }, 15000);
     }
     else if (correctAnswers == 1) {
         numberOfAnswers = document.getElementById("list2").getElementsByTagName("li").length;
         let correctAnswer = returnCorrectAnswerPosition("answer2");
         setTimeout(function() {
-            // Ocultar la animación de carga después de un tiempo de espera
+            // After a time of 15 seconds hide the loading animation and the feedback and create the svg chart
             document.getElementById('loading').style.display = 'none';
             document.getElementById('wildcardFeedback').style.display = 'none';
             createChart(numberOfAnswers, correctAnswer);
-            // Realiza la acción deseada aquí, por ejemplo, cargar contenido o realizar una solicitud AJAX.
         }, 15000);
     } else {
         numberOfAnswers = document.getElementById("list3").getElementsByTagName("li").length;
         let correctAnswer = returnCorrectAnswerPosition("answer3");
         setTimeout(function() {
-            // Ocultar la animación de carga después de un tiempo de espera
+            // After a time of 15 seconds hide the loading animation and the feedback and create the svg chart
             document.getElementById('loading').style.display = 'none';
             document.getElementById('wildcardFeedback').style.display = 'none';
             createChart(numberOfAnswers, correctAnswer);
-            // Realiza la acción deseada aquí, por ejemplo, cargar contenido o realizar una solicitud AJAX.
         }, 15000);
     }
     popup.style.display = "block";
@@ -234,7 +229,7 @@ function createChart(numberOfBars, correctAnswerPos) {
         percentages.push(probabilityByOption);
     }
     if (publicCorrect) {
-        let incrementCorrect = Math.floor(Math.random() * (25 - 10 + 1)) + 10; // numero random que genera un num del 10 al 25
+        let incrementCorrect = Math.floor(Math.random() * (25 - 10 + 1)) + 10; // random number from 10 to 25
         percentages[correctAnswerPos] += incrementCorrect;
         while (true) {
             let modIndex = Math.floor(Math.random() * percentages.length); // a number from 0 to lenght of the percentatges - 1
@@ -244,7 +239,7 @@ function createChart(numberOfBars, correctAnswerPos) {
             } 
         }
     } else {
-        let decrementCorrect = Math.floor(Math.random() * (25 - 10 + 1)) + 10; // numero random que genera un num del 10 al 25
+        let decrementCorrect = Math.floor(Math.random() * (25 - 10 + 1)) + 10; // random number from 10 to 25
         percentages[correctAnswerPos] -= decrementCorrect;
         while (true) {
             let modIndex = Math.floor(Math.random() * percentages.length); // a number from 0 to lenght of the percentatges - 1
@@ -255,30 +250,29 @@ function createChart(numberOfBars, correctAnswerPos) {
         }
     }
 
-    // Datos de ejemplo (porcentajes para cuatro barras y etiquetas correspondientes)
-    const etiquetas = ["A", "B", "C", "D"]; // Etiquetas correspondientes
+    const etiquetas = ["A", "B", "C", "D"];
 
-    // Configuración del gráfico
+    // Chart config
     const width = 410;
     const height = 300;
     const barSpacing = 20;
     const barWidth = (width - (barSpacing * (numberOfBars - 1))) / numberOfBars;
 
-    // Crea un elemento SVG usando D3.js
+    // Create a svg element using D3.js
     const svg = d3.select("#chart")
     .attr("width", width)
     .attr("height", height);
 
-    // Crea un filtro de sombra en CSS para aplicar a las barras
+    // Chart bars css shadow filter
     svg.append("defs").append("filter")
     .attr("id", "drop-shadow")
     .append("feDropShadow")
     .attr("dx", 4)
     .attr("dy", 4)
     .attr("stdDeviation", 0)
-    .attr("flood-color", "#42393b"); // Color de la sombra
+    .attr("flood-color", "#42393b"); // Color of the shadow
 
-    // Crea las barras del gráfico
+    // Create the bars of the chart
     for (let i = 0; i < numberOfBars; i++) {
     const x = i * (barWidth + barSpacing);
     const barHeight = height * (percentages[i] / 100);
@@ -289,23 +283,23 @@ function createChart(numberOfBars, correctAnswerPos) {
         .attr("width", barWidth - 5)
         .attr("height", barHeight)
         .attr("fill", "#ffc897")
-        .attr("stroke", "#42393b") // Color del borde
-        .attr("stroke-width", 3) // Ancho del borde
-        .attr("rx", 5) // Radio de la esquina X
-        .attr("ry", 5) // Radio de la esquina Y
-        .style("filter", "url(#drop-shadow)"); // Aplica la sombra
+        .attr("stroke", "#42393b") // Border color
+        .attr("stroke-width", 3) // Border width
+        .attr("rx", 5) // X corner radio
+        .attr("ry", 5) // Y corner radio
+        .style("filter", "url(#drop-shadow)"); // Apply the shadow
 
-    // Agrega etiquetas de porcentaje encima de las barras
+    // Add the percentage tags above the bars
     svg.append("text")
         .attr("x", x + barWidth / 2)
         .attr("y", height - barHeight - 40)
         .attr("text-anchor", "middle")
         .text((percentages[i]) + "%");
 
-    // Agrega etiquetas "A", "B", "C", "D" debajo de las barras
+    // Add the tags "A", "B", "C", "D" below of the bars
     svg.append("text")
         .attr("x", x + barWidth / 2)
-        .attr("y", height) // Ajusta la posición vertical según tus necesidades
+        .attr("y", height)
         .attr("text-anchor", "middle")
         .text(etiquetas[i]);
     }
@@ -320,31 +314,24 @@ function generatePublicProbability() {
       }
 }
 
-// checks if the session level is greater than 2
-checkSessionLevel();
-
 function fiftyPercentWildcard() {
     localStorage.setItem("50Wildcard", true);
     document.getElementById("50Wildcard").disabled = true;
-    // Obtener el id de la pregunta actual, reemplaza "question1" con el id correcto
-    const currentQuestionId = "question" + currentQuestion; // Reemplaza con el id de la pregunta actual
-
-    // Obtener todos los elementos <li> dentro de la lista de respuestas de la pregunta actual
+    // Obtains the current question id. Replaces "question1" with the correct id
+    const currentQuestionId = "question" + currentQuestion;
+    // Obtains all of the <li> elements inside the answers list of the actual question
     const listaRespuestas = document.querySelectorAll(`#${currentQuestionId} .answer-list li`);
 
-    // Contador para llevar el seguimiento de respuestas ocultas
+    // Counter to keep track of hidden responses
     let respuestasOcultas = 0;
-    console.log();
-    // Iterar a través de los elementos de la lista de respuestas de la pregunta actual
+
     listaRespuestas.forEach(function (item) {
-        // Obtener el input de tipo radio en cada elemento <li>
         const radioInput = item.querySelector('input[type="radio"]');
-        // Obtener el valor del input de tipo radio
         const valorRespuesta = radioInput.value;
 
-        // Verificar si el valor de la respuesta comienza con "-" y aún no se han ocultado dos respuestas
+        // Check if the value of the answer starts with "-" and two answers have not been hidden yet
         if (valorRespuesta.startsWith("-") && respuestasOcultas < 2) {
-            // Borrar el elemento <li>
+            // Delete the <li> element
             item.remove();
             respuestasOcultas++;
         }
@@ -353,9 +340,13 @@ function fiftyPercentWildcard() {
     const letters = ["A", "B", "C", "D"];
     const labelList = document.querySelectorAll(`#${currentQuestionId} .answer-list li`);
     for (let i = 0; i < labelList.length; i++) {
-        // Obtener el label en cada elemento <li>
+        // Obtains the <label> on each <li> element
         const labelElement = labelList[i].querySelector('label');
         const newText = letters[i] + labelElement.innerText.substring(1);
         labelElement.innerText = newText;
     }
 }
+
+// Checks if the session level is greater than 2
+// This is used to start the regresive counter for the first time
+checkSessionLevel();
