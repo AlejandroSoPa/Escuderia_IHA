3581907211f02e6722c512692fe9395a604340ed
Henry <henydoudoc@gmail.com>
2023-11-06 15:08:17 +0100

    Login Correccion 4

--- login.php
+++ login.php
@@ -6,6 +6,34 @@ $passWord = trans('password', $_SESSION['lang']);
 $btnSubmit = trans('submit', $_SESSION['lang']);
 $feedbackNotLog = trans('feedbackNotLog', $_SESSION['lang']);
 $logIn = trans('login', $_SESSION['lang']);
+$feedbackMessage = ''; // Variable para almacenar el feedback
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    // Leer el contenido del archivo JSON
+    $usuariosJSON = file_get_contents("logs.json");
+    $usuarios = json_decode($usuariosJSON, true);
+
+    // Inicializar una variable para indicar si se encontraron credenciales válidas
+    $credencialesValidas = false;
+
+    // Iterar a través de los usuarios y comparar las credenciales
+    foreach ($usuarios["usuarios"] as $user) {
+        if ($user["username"] === $username && $user["password"] === $password) {
+            // Las credenciales coinciden, usuario autenticado
+            $credencialesValidas = true;
+            header("Location: create.php");
+            exit;
+        }
+    }
+
+    // Verificar si se encontraron credenciales válidas
+    if (!$credencialesValidas) {
+        $feedbackMessage = $feedbackNotLog;
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -16,57 +44,27 @@ $logIn = trans('login', $_SESSION['lang']);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles.css">
     <title>Log in</title>
-    <script>
-        function mostrarFeedback() {
-            var username = document.getElementById("username").value;
-            var password = document.getElementById("password").value;
-
-            // Leer el contenido del archivo JSON
-            fetch("logs.json")
-                .then(response => response.json())
-                .then(data => {
-                    var credencialesValidas = false;
-
-                    // Iterar a través de los usuarios y comparar las credenciales
-                    data.usuarios.forEach(function(user) {
-                        if (user.username === username && user.password === password) {
-                            // Las credenciales coinciden, usuario autenticado
-                            credencialesValidas = true;
-                            window.location.href = "create.php";
-                        }
-                    });
-
-                    if (!credencialesValidas) {
-                        // Las credenciales no coinciden, muestra el mensaje de feedback
-                        var feedbackDiv = document.getElementById("feedbackLog");
-                        feedbackDiv.innerHTML = '<?php echo $feedbackNotLog; ?>';
-                        feedbackDiv.style.display = 'block'; // Mostrar la clase feedbackLog
-                    }
-                })
-                .catch(error => {
-                    console.error("Error al cargar el archivo JSON: " + error);
-                });
-        }
-    </script>
 </head>
 
 <body>
     <div class="log">
         <h2><?php echo $logIn ?></h2>
-        <form method="post" onsubmit="event.preventDefault(); mostrarFeedback();">
+        <form method="post">
             <div class="username">
                 <label for="username"><?php echo $userName ?>:</label>
                 <input type="text" id="username" name="username" required>
             </div>
             <br><br>
             <div class="pswd">
-                <label for="password"><?php echo $passWord ?>:</label>
+                <label for "password"><?php echo $passWord ?>:</label>
                 <input type="password" id="password" name="password" required>
             </div>
             <br><br>
             <button id="btnLog" type="submit"><?php echo $btnSubmit ?></button>
         </form>
-        <div class="feedbackLog" id="feedbackLog" style="display: none;"></div>
+        <div class="feedbackLog">
+            <?php echo $feedbackMessage; ?>
+        </div>
     </div>
 </body>
 
