989a598430c8e6706dcad6ec91a9ffd96e8433f4
Henry <henydoudoc@gmail.com>
2023-11-06 13:38:39 +0100

    Login Correccion

--- login.php
+++ login.php
@@ -7,20 +7,53 @@ $btnSubmit = trans('submit', $_SESSION['lang']);
 $feedbackNotLog = trans('feedbackNotLog', $_SESSION['lang']);
 $logIn = trans('login', $_SESSION['lang']);
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles.css">
     <title>Log in</title>
+    <script>
+        function mostrarFeedback() {
+            var username = document.getElementById("username").value;
+            var password = document.getElementById("password").value;
+
+            // Leer el contenido del archivo JSON
+            fetch("logs.json")
+                .then(response => response.json())
+                .then(data => {
+                    var credencialesValidas = false;
+
+                    // Iterar a través de los usuarios y comparar las credenciales
+                    data.usuarios.forEach(function(user) {
+                        if (user.username === username && user.password === password) {
+                            // Las credenciales coinciden, usuario autenticado
+                            credencialesValidas = true;
+                            window.location.href = "create.php";
+                        }
+                    });
+
+                    if (!credencialesValidas) {
+                        // Las credenciales no coinciden, muestra el mensaje de feedback
+                        var feedbackDiv = document.getElementById("feedbackLog");
+                        feedbackDiv.innerHTML = '<?php echo $feedbackNotLog; ?>';
+                        feedbackDiv.style.display = 'block'; // Mostrar la clase feedbackLog
+                    }
+                })
+                .catch(error => {
+                    console.error("Error al cargar el archivo JSON: " + error);
+                });
+        }
+    </script>
 </head>
 
 <body>
     <div class="log">
         <h2><?php echo $logIn ?></h2>
-        <form method="post">
+        <form method="post" onsubmit="event.preventDefault(); mostrarFeedback();">
             <div class="username">
                 <label for="username"><?php echo $userName ?>:</label>
                 <input type="text" id="username" name="username" required>
@@ -31,34 +64,9 @@ $logIn = trans('login', $_SESSION['lang']);
                 <input type="password" id="password" name="password" required>
             </div>
             <br><br>
-    
-                <button id="btnLog" type="submit"><?php echo $btnSubmit ?></button>
-
+            <button id="btnLog" type="submit"><?php echo $btnSubmit ?></button>
         </form>
-        <div class="feedbackLog">
-        <?php
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            $username = $_POST["username"];
-            $password = $_POST["password"];
-
-            // Leer el contenido del archivo JSON
-            $usuariosJSON = file_get_contents("logs.json");
-            $usuarios = json_decode($usuariosJSON, true);
-
-            // Iterar a través de los usuarios y comparar las credenciales
-            foreach ($usuarios["usuarios"] as $user) {
-                if ($user["username"] === $username && $user["password"] === $password) {
-                    // Las credenciales coinciden, usuario autenticado
-                    header("Location: create.php");
-                    exit;
-                } else {
-                    echo $feedbackNotLog;
-                }
-            }
-
-        }
-        ?>
-        </div>
+        <div class="feedbackLog" id="feedbackLog" style="display: none;"></div>
     </div>
 </body>
 
