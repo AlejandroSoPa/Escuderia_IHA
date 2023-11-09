ecbbfaa9e4b4641ddf562cf78fc590a608afc53b
Isaac <ifuriomartin.cf@iesesteveterradas.cat>
2023-11-09 01:29:32 +0100

    login bug fixed

--- login.php
+++ login.php
@@ -25,7 +25,7 @@ if(isset($_POST["username"])){
     foreach ($usuarios as $order => $valor) { 
         if($_POST["username"]==$order && $_POST["password"]==$valor){
             $_SESSION["login"] = "correcto";
-            header("Location:http://localhost:8080/create.php");
+            header("Location:./create.php");
         } else{
             $feedbackVisible = "visible";
         }
